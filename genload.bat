726ecb6d3ee42d97b5be16c78a29b456a449da8e
mihsu-clgx <73597758+mihsu-clgx@users.noreply.github.com>
2021-06-03 09:27:39 -0700

    Update genload.bat
    update job end

--- genload.bat
+++ genload.bat
@@ -2,7 +2,8 @@
 @SET db=%1
 @echo echo job start %%date%%-%%time%% > load-%db%.bat
 @dir *.csv|grep -v " 0 "|grep -v "File(s)"|awk "{print $5}"|grep ".csv"|sed "s/^/call load1tbl.bat %db% /;s/.csv//" >> load-%db%.bat
-@echo echo job   end %%date%%-%%time%% >> load-%db%.bat
+@echo CALL analyze-tbl.bat %db% >> load-%db%.bat
+@echo echo job end   %%date%%-%%time%% >> load-%db%.bat
 @TYPE load-%db%.bat
 @echo ... load-%db%.bat
 @GOTO EOF
